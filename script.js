ed3e828463088f9f02931a881dcaaf694fddcdb6
kalilakorin <kalila.korin@gmail.com>
2024-09-03 22:49:43 -0600

    Exercise 3c

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam1mcmltbWwiLCJhIjoiY20wZzdid3JyMTkweTJpb3J1YnJ6b3BkNiJ9.AIq8-_n3FX7v_45I0Ria3w";
+map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/jmfrimml/cm0nacj6v01b701pw7dvxenda", // style url
+  center: [-104.9, 39.7], // starting position [long, lat]
+  zoom: 7 // starting zoom
+});
+
 function parseSourceParams() {
-    const locationStr = window.location;
-    console.log("Location string: " + locationStr);
+  const locationStr = window.location;
+  console.log("Location string: " + locationStr);
 
-    const urlParams = new URLSearchParams(locationStr.search);
-    var source = urlParams.get('source');
-    console.log("Source variable: " + source);
+  const urlParams = new URLSearchParams(locationStr.search);
+  var source = urlParams.get("source");
+  console.log("Source variable: " + source);
 
-    if (source == "CIA")
-    {
-        document.getElementById("usa").href="https://www.cia.gov/the-world-factbook/countries/united-states/";
-    }
+  if (source == "winterpark") {
+    // Set center of the map
+    map.setCenter([-105.7748, 39.8473]);
+    // Set zoom level
+    map.setZoom(9);
+    // Update inter HTML
+    document.getElementById("skiText").innerHTML = "Ski Area: Winter Park";
+  }
+
+  if (source == "vail") {
+    map.setCenter([-106.319, 39.6446]);
+    map.setZoom(9);
+    document.getElementById("skiText").innerHTML = "Ski Area: Vail";
+  }
 }
